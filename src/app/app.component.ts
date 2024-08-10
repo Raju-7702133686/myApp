df03e9a99ec2d35d08a4b5f22f3206d19884566c
Raju <rajuprasad7919@gmail.com>
2024-08-10 14:46:57 +0530

    final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myApp';
+
+  public userForm: FormGroup= new FormGroup({
+    username: new FormControl('',[Validators.required, Validators.minLength(3), Validators.maxLength(10)]),
+    password: new FormControl('',[Validators.required, Validators.pattern('^[a-zA-Z0-9_-]{3,16}$')])
+
+  })
+
+  submit(){
+    console.log(this.userForm.value);
+  }
+
 }
